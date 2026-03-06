b52fae49ac45118fa5e31ceeb0af518aabccfa14
Akbar Bahodirov <bahodirovakbar572@gmail.com>
2026-03-06 16:26:16 +0500

    file

---
+++ backend/server.mjs
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+  
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/main.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/about.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT}`);
+}); 
