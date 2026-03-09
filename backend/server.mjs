cd343fae4f32e0ede243d5beb74397d7dfc7d37f
Akbar Bahodirov <bahodirovakbar572@gmail.com>
2026-03-09 15:55:54 +0500

    added title

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "../img")));
   
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../main.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 app.get("/about", (req, res) => {
