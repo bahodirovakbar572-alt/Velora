ff82096a0d1b03ad6f6a889398644fe76fef0b04
Akbar Bahodirov <bahodirovakbar572@gmail.com>
2026-03-09 16:26:43 +0500

    added file

--- backend/server.mjs
+++ backend/server.mjs
@@ -8,19 +8,22 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3030;
 
-app.use(express.static(path.join(__dirname, "../css")));
-app.use(express.static(path.join(__dirname, "../js")));
-app.use(express.static(path.join(__dirname, "../img")));
-  
+// Loyihaning asosiy papkasini aniqlaymiz (backend'dan bitta tepada)
+const root = path.join(__dirname, "..");
 
+// Statik papkalarni serverga ulaymiz
+app.use(express.static(root)); // Asosiy papka (index.html va main.css uchun)
+app.use('/img', express.static(path.join(root, 'img'))); // Rasmlar papkasi
+
+// Sahifalarni yuborish
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(root, "index.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "../about.html"));
+    res.sendFile(path.join(root, "about.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
-}); 
+    console.log(`Server: http://localhost:${PORT}`);
+});
